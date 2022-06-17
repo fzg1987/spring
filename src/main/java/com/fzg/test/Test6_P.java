e3a813913278a10c92bbbbbbbd0f16797925dbd2
fzg1987 <beginer@yeah.net>
2022-06-17 19:36:50 +0800

    工厂模式

--- src/main/java/com/fzg/test/Test6_P.java
+++ src/main/java/com/fzg/test/Test6_P.java
@@ -4,7 +4,7 @@ import com.fzg.entity.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test6_p {
+public class Test6_P {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-p.xml");
         Student student = context.getBean("student",Student.class);
