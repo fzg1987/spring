bf156df7b7d714f9b4bb7608cce0c8a43655a48b
fzg1987 <beginer@yeah.net>
2022-06-18 16:05:12 +0800

    Spring-ioc实例

--- src/main/java/com/fzg/test/Test10_IoC.java
+++ src/main/java/com/fzg/test/Test10_IoC.java
@@ -5,13 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test10_IoC {
-    private static double score = 90;
+    private static final double score = 90;
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-ioc.xml");
-//        String[] names = context.getBeanDefinitionNames();
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
         MyController controller = context.getBean("controller",MyController.class);
         String result = controller.doController(score);
         System.out.println(result);
